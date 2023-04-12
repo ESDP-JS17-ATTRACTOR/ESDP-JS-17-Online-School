194bd2047b5e1eab550a7cb91e1258ee0531cfaa
Almaz Balbaev <kujitengu@gmail.com>
2023-04-12 17:15:53 +0600

    #8 User's security methods
    added generateToken method
    added checkPassword method
    added hashPassword method

--- back/src/entities/user.entity.ts
+++ back/src/entities/user.entity.ts
@@ -1,4 +1,8 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { BeforeInsert, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import * as bcrypt from 'bcrypt';
+import * as crypto from 'crypto';
+
+const SALT_WORK_FACTOR = 10;
 
 @Entity()
 export class User {
@@ -42,4 +46,18 @@ export class User {
 
   @Column({ type: 'boolean', nullable: true })
   status: false;
+
+  async generateToken() {
+    this.token = crypto.randomUUID();
+  }
+
+  async checkPassword(password) {
+    return bcrypt.compare(password, this.password);
+  }
+
+  @BeforeInsert()
+  async hashPassword() {
+    const salt = await bcrypt.genSalt(SALT_WORK_FACTOR);
+    this.password = await bcrypt.hash(this.password, salt);
+  }
 }
