badb60e534b041647a88ab33888d1a5889136bc6
Almaz Balbaev <kujitengu@gmail.com>
2023-04-13 20:44:55 +0600

    #10 User Validation

--- back/src/users/users.controller.ts
+++ back/src/users/users.controller.ts
@@ -1,9 +1,16 @@
-import { Controller, NotFoundException, Post, Req } from '@nestjs/common';
+import {
+  Body,
+  ClassSerializerInterceptor,
+  Controller,
+  Post,
+  UseInterceptors,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from '../entities/user.entity';
-import { plainToClass } from 'class-transformer';
-import { validate } from 'class-validator';
+import { RegisterDto } from './dto/register.dto';
 
 @Controller('users')
 export class UsersController {
@@ -13,40 +20,11 @@ export class UsersController {
   ) {}
 
   @Post('register')
-  async registerUser(@Req() req) {
-    const existingUser = await this.userRepository.findOne({
-      where: {
-        email: req.body.email,
-      },
-    });
-    if (existingUser) {
-      throw new NotFoundException(
-        `User with email ${req.body.email} already exists`,
-      );
-    }
-    const user = this.userRepository.create({
-      email: req.body.email,
-      firstName: req.body.firstName,
-      lastName: req.body.lastName,
-      phoneNumber: req.body.phoneNumber,
-      country: req.body.country,
-      password: req.body.password,
-      avatar: req.body.avatar,
-    });
+  @UseInterceptors(ClassSerializerInterceptor)
+  @UsePipes(ValidationPipe)
+  async registerUser(@Body() body: RegisterDto) {
+    const user = this.userRepository.create(body);
     await user.generateToken();
-    const errors = await validate(user);
-
-    if (errors.length > 0) {
-      return errors.map((error) => {
-        return {
-          value: error.value,
-          property: error.property,
-          message: Object.values(error.constraints)[0],
-        };
-      });
-    } else {
-      await this.userRepository.save(user);
-    }
-    return plainToClass(User, user);
+    return this.userRepository.save(user);
   }
 }
